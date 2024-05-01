1fcb3c63fc95721779d6eee1f1342d3eaab76480
SowmyaRaju <sowmya.raju19@gmail.com>
2024-05-01 21:24:52 +1000

    Dependency injection - services

---
+++ FlightFinder.API/FlightFinder.API/Startup/ConfigureSwagger.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System.Runtime.CompilerServices;
+
+namespace FlightFinder.API.Startup
+{
+    public static class SwaggerConfiguration
+    {
+        public static WebApplication ConfigureSwagger(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            return app;
+        }
+    }
+   
+}
+
---
+++ FlightFinder.API/FlightFinder.API/Startup/DependencyInjectionSetup.cs
@@ -0,0 +1,16 @@
+﻿using FlightFinder.API.Services;
+using System.Runtime.CompilerServices;
+
+namespace FlightFinder.API.Startup
+{
+    public static class DependencyInjectionSetup
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddTransient<IFlightCentreService, FlightCentreService>();
+            return services;
+        }
+    }
+}
